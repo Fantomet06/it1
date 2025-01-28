a387de0387df95cba84e4f72631564b15f57b65e
Fantomet <44312327+Fantomet06@users.noreply.github.com>
2025-01-28 13:48:07 +0100

    changes

--- 50-spill/src/js/spill.js
+++ 50-spill/src/js/spill.js
@@ -9,13 +9,20 @@ const score1 = document.querySelector('#player1_score')
 
 button1.addEventListener('click', () => {
     if (turn.textContent.includes("1")) {
+
         turn.textContent = "Spiller 2 sin tur"
-        score1.textContent = parseInt(score1.textContent) + getRandomNumber();
+        let new_score = parseInt(score1.textContent) + getRandomNumber();
+        if (new_score <= 50) {
+            score1.textContent = new_score;
+        } else {
+            score1.textContent = parseInt(score1.textContent)-(new_score - 50);
+        }
+
     } else {
         alert("Det er ikke din tur!")
     }
 
-    if (parseInt(score1.textContent) >= 50) {
+    if (parseInt(score1.textContent) === 50) {
         alert("Spiller 1 vant!")
     }
 });
@@ -26,12 +33,18 @@ const score2 = document.querySelector('#player2_score')
 button2.addEventListener('click', () => {
     if (turn.textContent.includes("2")) {
         turn.textContent = "Spiller 1 sin tur"
-        score2.textContent = parseInt(score2.textContent) + getRandomNumber();
+        let new_score = parseInt(score2.textContent) + getRandomNumber();
+        if (new_score <= 50) {
+            score2.textContent = new_score;
+        } else {
+            score2.textContent = parseInt(score2.textContent)-(new_score - 50);
+        }
+        
     } else {
         alert("Det er ikke din tur!")
     }
 
-    if (parseInt(score2.textContent) >= 50) {
+    if (parseInt(score2.textContent) === 50) {
         alert("Spiller 2 vant!")
     }
 });
